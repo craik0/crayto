b249e5701b76fa73e1cb8835440dbe02d8ab4911
craik0 <tyreeisaiah575@gmail.com>
2025-10-21 22:12:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const minutes = String(now.getMinutes()).padStart(2, "0");
         const seconds = String(now.getSeconds()).padStart(2, "0");
 
-        const defaultVideo = "https://youtu.be/bmaZdtWTpQo";
+        const defaultVideo = "https://www.youtube.com/watch?v=K8yLk1wavRc&t=10s";
         let videoLink = defaultVideo;
 
-        const label = `<a id="custom-message" href="${defaultVideo}" target="_blank" rel="noopener noreferrer">Schedule 1 is PEAK gaming. OUT NOW!!!</a>`;
+        const label = `<a id="custom-message" href="${defaultVideo}" target="_blank" rel="noopener noreferrer">SUPER MARIO GALAXY 2 FOR NINTEDO SWITCH DAY 2 OUT NOW</a>`;
 
         timerContainer.innerHTML = `${day} ${month} ${year} ${hours}:${minutes}:${seconds} | ${label}`;
     }
